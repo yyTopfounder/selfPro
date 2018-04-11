dfe61ee51f15639dda7dd354c7a340b8e3a97e67
yyTopfounder <2084925399@qq.com>
2018-04-11 16:57:26 +0800

    详细站点添加

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,6 +10,8 @@ export class HomePage {
   newMove:number = 0;
   themColor:string = '#30d05f';
   stationContrain:boolean = false;
+  clickLineName:string;
+  lineStation:Array<string> = [];
 
   constructor(public navCtrl: NavController,private el:ElementRef,public menuCtrl: MenuController) {
     //消息轮播动画
@@ -43,13 +45,24 @@ export class HomePage {
     }
     this.menuCtrl.open();
     this.stationContrain = false;
+    this.clickLineName = 0;
   }
 
   //侧边菜单地铁详细站点显示隐藏切换
   toggleStation(event:Event){
-    console.log(event)
-    if (/\d+号线/.test(event.target.innerHTML)){
+    console.log(event);
+    if(/\d+号线/.test(event.target.innerText)){
+      this.stationContrain = true;
+      this.clickLineName = /\d+/.exec(event.target.innerText)[0];
+      switch (this.clickLineName){
+        case '1': this.lineStation = ["富锦路","友谊西路","宝安公路","共富新村","呼兰路","通河新村","共康路","彭浦新村","汶水路","上海马戏城",
+          "延长路","中山北路","上海火车站","汉中路","新闸路","人民广场","黄陂南路","陕西南路","常熟路","衡山路",
+          "徐家汇","上海体育馆","漕宝路","上海南站","锦江乐园","莲花路","外环路","莘庄"];break;
+        case '2': this.lineStation = ["浦东机场","海天三路","远东大道","凌空路","川沙","华夏东路","创新中路","唐镇","广兰路","金科路",
+          "张江高科","龙阳路","世纪公园","东昌路","陆家嘴","南京东路","人民广场","南京西路","静安寺","江苏路",
+          "中山公园","娄山关路","咸宁路","北新泾","淞虹路","虹桥航站楼","虹桥火车站","徐泾东"];break;
 
+      }
     }
   }
 
