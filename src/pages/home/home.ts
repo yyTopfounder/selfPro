65a50b97cc7767f0377dda59f6a01bbb53031bfa
yyTopfounder <2084925399@qq.com>
2018-04-16 17:01:38 +0800

    http

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component,ViewChild,ElementRef } from '@angular/core';
-import { NavController,Slides,MenuController } from 'ionic-angular';
+import {Component,ViewChild,ElementRef} from '@angular/core';
+import {NavController,Slides,MenuController} from 'ionic-angular';
+import {HttpClientModule} from "@angular/common/http";
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'page-home',
@@ -13,8 +15,52 @@ export class HomePage {
   clickLineName:string;
   lineStation:Array<string> = [];
 
-  constructor(public navCtrl: NavController,private el:ElementRef,public menuCtrl: MenuController) {
-    //消息轮播动画
+  constructor(public navCtrl: NavController,private el:ElementRef,public menuCtrl: MenuController,private http:HttpClientModule) {
+  }
+
+  ngOnInit():void{
+    this.messageBarAnimation();
+    console.log(this.http)
+    this.http.get("http://localhost:3000/home").subscribe(response =>{
+      console.log(response)
+    });
+
+  }
+
+  //侧边菜单导航
+  openMenu(flag:boolean):void {
+    if(flag){
+      this.menuCtrl.enable(true, 'filterMenu');
+      this.menuCtrl.enable(false, 'mangeMenu');
+    }else{
+      this.menuCtrl.enable(false, 'filterMenu');
+      this.menuCtrl.enable(true, 'mangeMenu');
+    }
+    this.menuCtrl.open();
+    this.stationContrain = false;
+    this.clickLineName = '';
+  }
+
+  //侧边菜单地铁详细站点显示隐藏切换
+  toggleStation(event):void{
+    console.log(event);
+    if(/\d+号线/.test(event.target.innerText)){
+      this.stationContrain = true;
+      this.clickLineName = /\d+/.exec(event.target.innerText)[0];
+      switch (this.clickLineName){
+        case '1': this.lineStation = ["富锦路","友谊西路","宝安公路","共富新村","呼兰路","通河新村","共康路","彭浦新村","汶水路","上海马戏城",
+          "延长路","中山北路","上海火车站","汉中路","新闸路","人民广场","黄陂南路","陕西南路","常熟路","衡山路",
+          "徐家汇","上海体育馆","漕宝路","上海南站","锦江乐园","莲花路","外环路","莘庄"];break;
+        case '2': this.lineStation = ["浦东机场","海天三路","远东大道","凌空路","川沙","华夏东路","创新中路","唐镇","广兰路","金科路",
+          "张江高科","龙阳路","世纪公园","东昌路","陆家嘴","南京东路","人民广场","南京西路","静安寺","江苏路",
+          "中山公园","娄山关路","咸宁路","北新泾","淞虹路","虹桥航站楼","虹桥火车站","徐泾东"];break;
+
+      }
+    }
+  }
+
+  //消息栏轮播动画
+  messageBarAnimation():void{
     setInterval(()=>{
       if(this.newMove+60 === 0){
         this.el.nativeElement.querySelector('.newItem').style.marginTop = '0px';
@@ -32,38 +78,6 @@ export class HomePage {
         },80)
       }
     },5000);
-  }
-
-  //侧边菜单导航
-  openMenu(flag:boolean) {
-    if(flag){
-      this.menuCtrl.enable(true, 'filterMenu');
-      this.menuCtrl.enable(false, 'mangeMenu');
-    }else{
-      this.menuCtrl.enable(false, 'filterMenu');
-      this.menuCtrl.enable(true, 'mangeMenu');
-    }
-    this.menuCtrl.open();
-    this.stationContrain = false;
-    this.clickLineName = '';
-  }
-
-  //侧边菜单地铁详细站点显示隐藏切换
-  toggleStation(event){
-    console.log(event);
-    if(/\d+号线/.test(event.target.innerText)){
-      this.stationContrain = true;
-      this.clickLineName = /\d+/.exec(event.target.innerText)[0];
-      switch (this.clickLineName){
-        case '1': this.lineStation = ["富锦路","友谊西路","宝安公路","共富新村","呼兰路","通河新村","共康路","彭浦新村","汶水路","上海马戏城",
-          "延长路","中山北路","上海火车站","汉中路","新闸路","人民广场","黄陂南路","陕西南路","常熟路","衡山路",
-          "徐家汇","上海体育馆","漕宝路","上海南站","锦江乐园","莲花路","外环路","莘庄"];break;
-        case '2': this.lineStation = ["浦东机场","海天三路","远东大道","凌空路","川沙","华夏东路","创新中路","唐镇","广兰路","金科路",
-          "张江高科","龙阳路","世纪公园","东昌路","陆家嘴","南京东路","人民广场","南京西路","静安寺","江苏路",
-          "中山公园","娄山关路","咸宁路","北新泾","淞虹路","虹桥航站楼","虹桥火车站","徐泾东"];break;
-
-      }
-    }
   }
 
 }
